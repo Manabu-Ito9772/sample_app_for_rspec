051bfd75eac335706369a6aa8d58ad0f69b250d6
Manabu Ito <m.ito09017@gmail.com>
2020-11-27 14:34:57 +0900

    [Add] RSpec モデルスペック

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with all attributes' do
+    task = build(:task)
+    expect(task).to be_valid
+    expect(task.errors).to be_empty
+  end
+
+  it 'is invalid without title' do
+    task = build(:task, title: nil)
+    expect(task).to be_invalid
+    expect(task.errors[:title]).to include("can't be blank")
+  end
+
+  it 'is invalid without status' do
+    task = build(:task, status: nil)
+    expect(task).to be_invalid
+    expect(task.errors[:status]).to include("can't be blank")
+  end
+
+  it 'is invalid with a duplicate title' do
+    create(:task)
+    task = build(:task)
+    expect(task).to be_invalid
+    expect(task.errors[:title]).to include("has already been taken")
+  end
+
+  it 'is valid with another title' do
+    create(:task)
+    task = build(:task, title: "Test title2")
+    expect(task).to be_valid
+    expect(task.errors).to be_empty
+  end
 end
